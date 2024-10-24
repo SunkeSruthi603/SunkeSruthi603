a0f8ba045281ab15a6d3a729c9cc4cacf59ad80b
SunkeSruthi603 <sunkesruthi323@gmail.com>
2024-10-24 14:29:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View ,ToastAndroid ,TouchableOpacity} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Undertrials Project</Text>
+      <Text>my first Project</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    color:"#304ffe",
+    fontSize: 20,
+    padding: 20
+  },
+
+  button: {
+    borderRadius: 10,
+    backgroundColor:"#29b0f0",
+    width:150,
+    height:150,
+    justifyContent:'center',
+    alignItems:"center"
+  }
+}
+);
